c484f9e970555aba17ea91500f58f6ce6b44ccab
José Wanderson <wandersonjose75@gmail.com>
2026-06-19 23:34:41 -0300

    [JOKENPO] Fixes get random number using sys_time

--- x86/src/jokenpo/random.asm
+++ x86/src/jokenpo/random.asm
@@ -1,28 +1,34 @@
 section .bss
     seed RESW 0x01
 
+section .data
+    time dq 0
+
 section .text
 seed_init:
-    MOV EAX, 0x0D    ; sys_time
-    XOR EBX, EBX
+    MOV EAX, 13    ; sys_time
+    XOR EDI, EDI
+    ; MOV RDI, time
     INT 0x80
+
     MOV [seed], AX
+    ; MOV DWORD EAX, [time]
     ret
 
 get_random:
     ; Formula LCG: Semente = (Semente * 1103515245 + 12345)
-    mov eax, [seed]
-    imul eax, 1103515245
-    add eax, 12345
-    mov [seed], eax  ; Atualiza a semente para a próxima chamada
+    MOV EAX, [seed]
+    IMUL EAX, 1103515245
+    ADD EAX, 12345
+    MOV [seed], EAX  ; Atualiza a semente para a próxima chamada
 
     ; Limitar o número para o intervalo de 1 a 3 usando a instrução DIV
     ; O resto da divisão por 3 (EAX mod 3) resultará em 0, 1 ou 2.
     ; Somando 1 no final, o resultado vira 1, 2 ou 3.
-    xor edx, edx        ; Limpa EDX antes da divisao (importante!)
-    mov ecx, 3          ; Queremos um intervalo de 3 possibilidades
-    div ecx             ; Divide EDX:EAX por ECX. O resto vai para EDX!
+    xor EDX, EDX        ; Limpa EDX antes da divisao (importante!)
+    MOV ECX, 3          ; Queremos um intervalo de 3 possibilidades
+    DIV ECX             ; Divide EDX:EAX por ECX. O resto vai para EDX!
 
-    mov eax, edx        ; Move o resto (0, 1 ou 2) para EAX
-    inc eax             ; Soma 1 para virar (1, 2 ou 3)
+    MOV EAX, EDX        ; Move o resto (0, 1 ou 2) para EAX
+    INC EAX             ; Soma 1 para virar (1, 2 ou 3)
     ret            ; AX now holds your random number
